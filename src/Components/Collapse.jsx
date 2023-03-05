0049830cdabaa8b14783d8814ad617f8e2ef13fb
anom35 <freelance35830@gmail.com>
2023-03-05 22:28:36 +0100

    commit

--- src/Components/Collapse.jsx
+++ src/Components/Collapse.jsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react'
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import "../Styles/Collapse.css"
 
 function Collapse(props) {
     const [contentVisible, setContentVisible] = useState(false)
-    const affContent = () => {
-        setContentVisible(!contentVisible)
-    }
-    const classContent = (contentVisible ? "visible" : "hidden") + " collapse"
-    const classChevron = (contentVisible ? faChevronUp : faChevronDown)
+    const affContent = () => { setContentVisible(!contentVisible) }
+
+    const collapseContent = (contentVisible ? "visible" : "hidden") + " collapse"
+    const collapseChevron = (contentVisible ? faChevronUp : faChevronDown)
 
     return (
         <div className='collapse'>
             <p className='collapse__header' onClick={affContent}>
                 <span>{props.title}</span>
-                <i className={classChevron} />
+                <div className="chevronValue">
+                    <FontAwesomeIcon 
+                        key={"co_"+Math.random().toString()} 
+                        icon={collapseChevron} 
+                    />
+                </div>
             </p>
-            <div className={classContent}>
+            <div className={collapseContent}>
                 <p>{props.content}</p>
             </div>
         </div>
