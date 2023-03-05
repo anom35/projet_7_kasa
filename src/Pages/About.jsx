7a6083da2a385c6f36c8f9158f82dd752d7aa96f
anom35 <freelance35830@gmail.com>
2023-03-05 12:16:39 +0100

    About

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import Shaping from "../Components/Shaping"
+import Navbar from "../Components/Navbar"
+import Footer from "../Components/Footer"
+
+import "../Styles/About.css"
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <Shaping>
+        <Navbar />
+        <div className='about'>
+          <span>404</span>
+          <h4>Oups! La page que vous demandez n'existe pas.</h4>
+          <Link to="/">Retourner sur la page d’accueil</Link>
+        </div>
+      </Shaping>
+      <Footer />
+    </div>
   )
 }
 
