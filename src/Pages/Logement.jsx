7100246bbef256c50179ae1e22d2a8b32671388c
anom35 <freelance35830@gmail.com>
2023-03-05 17:58:16 +0100

    commit

--- src/Pages/Logement.jsx
+++ src/Pages/Logement.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+import Navbar from "../Components/Navbar"
+import Banner from '../Components/Banner'
+import Footer from "../Components/Footer"
+import Shaping from "../Components/Shaping"
+// import Carousel from '../Components/Carousel'
+// import Collapse from '../Components/Collapse'
+
+import "../Styles/Logement.css"
 
 function Logement() {
+
+  const idLogement = useSearchParams().get('_id')
+
   return (
-    <div>Logement</div>
+    <div>
+      <Shaping>
+        <Navbar />
+        <Banner imageUrl="logement"/>
+      </Shaping>
+      <Footer />
+    </div>
   )
 }
 
