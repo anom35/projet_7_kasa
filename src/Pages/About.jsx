aafc150588d846d49833f2ffa85405fe2fedd966
anom35 <freelance35830@gmail.com>
2023-03-06 16:45:58 +0100

    commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import { Link } from "react-router-dom"
 import Shaping from "../Layout/Shaping"
 import Navbar from "../Layout/Navbar"
 import Banner2 from '../Components/Banner2'
 import Footer from "../Layout/Footer"
+import Collapse from '../Components/Collapse'
 
 import "../Styles/About.css"
 
 function About() {
+  const respect = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
   return (
-    <div>
+    <div className='flex'>
       <Shaping>
         <Navbar />
         <Banner2 />
         <div className='about'>
-          <span>About</span>
-          <h4>Oups! La page que vous demandez n'existe pas.</h4>
-          <p><Link to="/">Retourner sur la page d’accueil</Link></p>
+          <div className='paramsCollapse'><Collapse title="Fiabilité" content="" /></div>
+          <div className='paramsCollapse'><Collapse title="Respect" content={respect} /></div>
+          <div className='paramsCollapse'><Collapse title="Service" content=" " /></div>
+          <div className='paramsCollapse'><Collapse title="Responsabilité" content="" /></div>
         </div>
       </Shaping>
       <Footer />
