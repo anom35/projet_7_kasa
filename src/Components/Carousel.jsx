f0b22143c0c438efb456279db6684bb3b1d95938
anom35 <freelance35830@gmail.com>
2023-03-13 13:15:54 +0100

    del pos img carousel mobile

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -9,30 +9,41 @@ function Carousel({pictures}) {
   const nbrePictures = pictures.length-1 // nombre max d'images
   if (cpt < 0) setCpt(nbrePictures)
   if (cpt > nbrePictures) setCpt(0)
-
-  return (
-    <div className='carousel'>
-
-      {/* emplacement image du carousel */}
-      <div className='div-image'>
-        <img src={pictures[cpt]} className="classImage" key={"car-"+cpt} alt={"photo "+cpt} />
+  
+  if (nbrePictures === 0) {
+    return (
+      <div className='carousel'>
+        {/* emplacement image du carousel */}
+        <div className='div-image'>
+          <img src={pictures[cpt]} className="classImage" key={"car-"+cpt} alt={"photo "+cpt} />
+        </div>
       </div>
+    )
+  } else { 
+    let suite =""
+    if (window.screen.width > 450) {
+      suite = <p>{cpt+1}/{nbrePictures+1}</p>
+    } else suite = ""
+    
+    return(
+      <div className='carousel'>
 
-      {/* boutons avec chevrons pour changer d'image et incrémente ou décrémente le compteur */}
-      <div>
-        <button onClick={() => setCpt(cpt - 1)}>{cpt}
-          <img src={chevronGauche} className='classChevronGauche' alt={'flèche gauche pour changer de photo '+cpt} />
-        </button>
-        <button onClick={() => setCpt(cpt + 1)}>{cpt}
-          <img src={chevronDroit} className='classChevronDroit' alt={'flèche droite pour changer de photo '+cpt} />
-        </button>
+        <div className='div-image'>
+          <img src={pictures[cpt]} className="classImage" key={"car-"+cpt} alt={"photo "+cpt} />
+        </div>
+
+        <div>
+          <button onClick={() => setCpt(cpt - 1)}>{cpt}
+            <img src={chevronGauche} className='classChevronGauche' alt={'flèche gauche pour changer de photo '+cpt} />
+          </button>
+          <button onClick={() => setCpt(cpt + 1)}>{cpt}
+            <img src={chevronDroit} className='classChevronDroit' alt={'flèche droite pour changer de photo '+cpt} />
+          </button>
+        </div>
+        {suite}
       </div>
-
-      {/* affiche le n° de l'image en cours / nbre max */}
-      <p>{cpt+1}/{nbrePictures+1}</p>
-
-    </div>
-  )
+    )
+  }
 }
 
 export default Carousel
