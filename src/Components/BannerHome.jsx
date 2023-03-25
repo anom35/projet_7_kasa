58b73d9524a52779b0e6a0b7468ddb9181c6db76
Stéphane Hatchikian <111690614+anom35@users.noreply.github.com>
2023-03-25 19:43:12 +0100

    Last modifs

--- src/Components/BannerHome.jsx
+++ src/Components/BannerHome.jsx
@@ -4,7 +4,9 @@ import '../Styles/Banner.css'
 // bannier pour la page d'accueil
 function BannerHome() {
   return (
-    <div className="banner banner-commun"><h1>Chez vous, partout et ailleurs</h1></div>
+    <div className="banner banner-commun">
+      <h1>Chez vous, partout et ailleurs</h1>
+    </div>
   )
 }
 
--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -5,8 +5,8 @@ import "../Styles/Footer.css"
 function Footer() {
     return (
         <div className='footer'>
-            <img src={logo} alt='Logo de bas de page' />
-            <p>© 2020 Kasa. All rights reserved</p>
+            <div className='img_footer'><img src={logo} alt='Logo de bas de page' /></div>
+            <div className='arr'><p>© 2020 Kasa. All rights reserved</p></div>
         </div>
     )
 }
