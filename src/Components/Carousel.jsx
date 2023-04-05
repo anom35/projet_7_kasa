ba558d16abdc9b610c8842302da7cd590e2d6702
Stéphane Hatchikian <111690614+anom35@users.noreply.github.com>
2023-04-05 06:47:07 +0200

    Optimisation du code

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -7,42 +7,39 @@ import "../Styles/Carousel.css"
 
 function Carousel({pictures}) { //reçois un tableau avec les URL des images
 
-  const [cpt, setCpt] = useState(0)  // variable de compteur, défini au départ sur 0
-  const nbrePictures = pictures.length-1 // nombre max d'images
+    const [index, setIndex] = useState(0)  // variable de compteur, défini au départ sur 0
+    const totalPictures = pictures.length-1 // nombre max d'images
 
-  if (cpt < 0) setCpt(nbrePictures)  // si inférieur à zéro, défini le nombre d'images max
-  if (cpt > nbrePictures) setCpt(0)  // si supérieur au max d'images, alors met à zéro
-  
-  if (nbrePictures === 0) {  // pour le cas ou il n'y a qu'une image (pas de flèches ni de compteur d'images)
-    return (
-      <div className='carousel'>
-        {/* emplacement image du carousel */}
-        <div className='div-image'>
-          <img src={pictures[cpt]} className="classImage" key={"car-"+cpt} alt={"photo "+cpt} />
-        </div>
-      </div>
-    )
-  } else {   // pour le cas ou il y a plusieurs images
+    if (index < 0) setIndex(totalPictures)  // si inférieur à zéro, défini le nombre d'images max
+    if (index > totalPictures) setIndex(0)  // si supérieur au max d'images, alors met à zéro
+    
     return(
-      <div className='carousel'>
+        <div className='carousel'>
 
-        <div className='div-image'>  {/* affiche image */}
-          <img src={pictures[cpt]} className="classImage" key={"car-"+cpt} alt={"photo "+cpt} />
-        </div>
+            <div className='div-image'>  {/* affiche image */}
+                <img src={pictures[index]} className="classImage" key={"car-"+index} alt={"photo "+index} />
+            </div>
 
-        <div> {/* les boutons qui afficherons les chevrons */}
-          <button onClick={() => setCpt(cpt - 1)}>{cpt}
-            <img src={chevronGauche} className='classChevronGauche' alt={'flèche gauche pour changer de photo '+cpt} />
-          </button>
-          <button onClick={() => setCpt(cpt + 1)}>{cpt}
-            <img src={chevronDroit} className='classChevronDroit' alt={'flèche droite pour changer de photo '+cpt} />
-          </button>
+            {/* si plus d'une image, alors ce code sera exécuté */}
+            {totalPictures > 0 && ( 
+                <div> {/* les boutons qui afficherons les chevrons */}
+                    <button onClick={() => setIndex(index - 1)}>{index}
+                        <img src={chevronGauche} className='classChevronGauche' alt={'flèche gauche pour changer de photo '+index} />
+                    </button>
+                    <button onClick={() => setIndex(index + 1)}>{index}
+                        <img src={chevronDroit} className='classChevronDroit' alt={'flèche droite pour changer de photo '+index} />
+                    </button>
+                </div>
+            )}
+            {totalPictures > 0 && (
+                <div className='div-compteur'> {/* compteur d'images */}
+                    <p className='compteurImages'>
+                        {index+1}/{totalPictures+1}
+                    </p>
+                </div>
+            )}
         </div>
-        {/* compteur d'images */}
-        <p className='compteurImages'>{cpt+1}/{nbrePictures+1}</p>
-      </div>
     )
-  }
 }
 
 export default Carousel
