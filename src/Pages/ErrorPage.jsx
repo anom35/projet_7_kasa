fd1847f3b09351ca0dfd8b88a9fece9a159a2160
Stéphane Hatchikian <111690614+anom35@users.noreply.github.com>
2023-04-05 09:34:42 +0200

    Optimisation

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -7,21 +7,19 @@ import Footer from "../Layout/Footer"
 import "../Styles/ErrorPage.css"
 
 function ErrorPage() {
-  return (
-    <Fragment>
-
-      <Shaping>
-        <Navbar />
-        <div className='error commun-error'>
-          <span>404</span>
-          <h4>Oups! La page que vous demandez n'existe pas.</h4>
-          <p><Link to="/">Retourner sur la page d’accueil</Link></p>
-        </div>
-      </Shaping>
-      
-      <Footer />
-    </Fragment>
-  )
+    return (
+        <Fragment>
+            <Shaping>
+                <Navbar />
+                <div className='error commun-error'>
+                    <span>404</span>
+                    <h4>Oups! La page que vous demandez n'existe pas.</h4>
+                    <p><Link to="/">Retourner sur la page d’accueil</Link></p>
+                </div>
+            </Shaping>
+            <Footer />
+        </Fragment>
+    )
 }
 
 export default ErrorPage
